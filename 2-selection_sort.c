5df8897ab585300a189fe4ea8e70d35501fff6da
Nomee07 <gigabanomfundo@gmail.com>
2023-11-21 15:57:44 -0800

    Sorting algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,28 +1,6 @@
 #include "sort.h"
 
 /**
- * write_array - Helper function to write an array to stdout
- * @array: Pointer to the array to be written
- * @size: Size of the array
- */
-void write_array(int *array, size_t size)
-{
-	char buffer[20];
-	size_t i;
-
-	for (i = 0; i < size; i++)
-	{
-		snprintf(buffer, sizeof(buffer), "%d", array[i]);
-		write(STDOUT_FILENO, buffer, strlen(buffer));
-
-		if (i < size - 1)
-			write(STDOUT_FILENO, ", ", 2);
-	}
-
-	write(STDOUT_FILENO, "\n", 1);
-}
-
-/**
  * selection_sort - Sorts an array of integers in ascending order
  * @array: Pointer to the array to be sorted
  * @size: Size of the array
@@ -30,29 +8,26 @@ void write_array(int *array, size_t size)
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
-	int temp;
+	int aux = 0;
+	size_t i = 0, j = 0, pos = 0;
 
-	if (!array || size <= 1)
+	if (array == NULL || size == 0)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (; i < size - 1; i++)
 	{
-		min_index = i;
-
+		pos = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
-				min_index = j;
+			if (array[j] < array[pos])
+				pos = j;
 		}
-
-		if (min_index != i)
+		if (pos != i)
 		{
-			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
-
-			write_array(array, size);
+			aux = array[i];
+			array[i] = array[pos];
+			array[pos] = aux;
+			print_array(array, size);
 		}
 	}
 }
