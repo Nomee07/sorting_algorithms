51c40889db069bffbef98aa5cca30a2baa406f73
Nomee07 <gigabanomfundo@gmail.com>
2023-11-21 15:49:46 -0800

    Sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,60 +1,29 @@
 #include "sort.h"
 
 /**
- * write_array - Helper function to write an array to stdout
- * @array: Pointer to the array to be written
- * @size: Size of the array
- */
-
-void write_array(int *array, size_t size)
-{
-	char buffer[20];
-	size_t i;
-
-	for (i = 0; i < size; i++)
-	{
-		snprintf(buffer, sizeof(buffer), "%d", array[i]);
-		write(STDOUT_FILENO, buffer, strlen(buffer));
-
-	if (i < size - 1)
-		write(STDOUT_FILENO, ", ", 2);
-	}
-	write(STDOUT_FILENO, "\n", 1);
-}
-
-/**
- * bubble_sort - Sorts an array of integers in ascending order
- * @array: Pointer to the array to be sorted
- * @size: Size of the array
- */
-
+  * bubble_sort - A function for bubble sorting.
+  * @array: The array to sort.
+  * @size: THe length of the aaray.
+  * Return: Nothing.
+  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-	int swapped;
+	size_t i = 0, j = 0;
+	int aux = 0;
 
-	if (!array || size <= 1)
+	if (array == NULL || size == 0)
 		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (; i < size - 1; i++)
 	{
-		swapped = 0;
-
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-
-				swapped = 1;
-				write_array(array, size);
+				aux = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = aux;
+				print_array(array, size);
 			}
 		}
-
-		if (!swapped)
-			break;
 	}
 }
